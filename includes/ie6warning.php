1b0bf98de83f63c4c71c870ad79618c50bf78b3c
Jacques Maes <jacmaes@gmail.com>
2011-11-20 20:12:17 +0100

    Getting closer...

---
+++ includes/ie6warning.php
@@ -0,0 +1,6 @@
+<?php if (strtolower(getenv('browser')) . getenv('version') == 'ie6') { ?>
+<div class="iewarning">
+<p><b>Nota Importante :</b> El navegador que usas para consultar esta página Web &mdash;Internet Explorer 6&mdash; fue lanzado en 2001 y es demasiado antiguo para poder mostrar esta página correctamente. Internet ha cambiado mucho desde entonces, pero tu navegador no. Te aconsejamos descargar un navegador más moderno y seguro como <a href="http://www.google.es/chrome">Google Chrome</a> o <a href="http://mozilla-europe.org/es/firefox/">Mozilla Firefox</a> para una mejor experiencia en hiedracenters.com, y cualquier otra Web. Es sencillo, rápido, y gratuito. No esperes más, nos lo agradecerás.</p>
+<p>¿No sabes lo que es un "navegador"? Deja que te lo explique el propio Google <a href="http://www.whatbrowser.org/es/">aquí</a> en un minuto.</p>
+</div>
+<?php }  ?>
---
+++ index.php
@@ -0,0 +1,80 @@
+<?php 
+// include CSS Crush, the PHP-based CSS preprocessor available here: http://the-echoplex.net/csscrush/
+require_once 'assets/crush/CssCrush.php'; 
+$options = array(
+    'versioning' => true,
+	'boilerplate' => true,
+	'debug' => false, // set it to true to view unminified css and debug in Web Inspector
+);
+// you might have to chmod your css directory to allow CSS Crush to write the compiled file correctly
+$compiled_file = CssCrush::file( 'assets/css/main.css', $options );
+?>
+<!doctype html>
+<html lang="es" class="<?php echo strtolower(getenv('browser')) . getenv('version'); ?> <?php echo strtolower(getenv('browser')); ?> <?php echo strtolower(getenv('os'));
+// browser and os detection classes generated through .htaccess file, used mainly to target our good friend IE in CSS ?>">
+<head>
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<title></title>
+<meta http-equiv="cleartype" content="on">
+<meta name="HandheldFriendly" content="True">
+<meta name="MobileOptimized" content="320">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<?php if (strtolower(getenv('browser')) . getenv('version') != 'ie6') { // serve main css file compiled and minified by CSS Crush to all but IE6. Extra CSS files are imported automatically in main CSS file with @import ?>
+<link rel="stylesheet" href="<?php echo $compiled_file; ?>" media="screen, projection">
+<?php } else { // serve universal stylesheet for IE6 ?>
+<link rel="stylesheet" href="http://universal-ie6-css.googlecode.com/files/ie6.1.1.css" media="screen, projection">
+<?php } ?>
+</head>
+<body>
+
+
+<header>
+</header>
+
+<div id="main" role="main">
+</div>
+
+
+
+<footer>
+</footer>
+
+<?php include "includes/ie6warning.php"; // warn ie6 users that they must upgrade or face nuclear war ?>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+<?php if (strtolower(getenv('browser')) . getenv('version') != 'ie6') { // serve main javascript with cache-busting timestamp to all but error-prone IE6 ?>
+<script type="text/javascript" src="/assets/js/site.<?php echo filemtime('/path/to/assets/js/site.js') ?>.js"></script>
+<?php } ?>
+
+<!--
+ ===================================================================================
+ HTML5 + Media Queries polyfills for IE7 and IE8, excluding IE on mobile devices
+ ===================================================================================
+ -->
+  
+<!--[if (lt IE 9) & (!IEMobile)]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<script type="text/javascript" src="/assets/js/respond.js"></script>
+<![endif]-->
+
+
+<!--   
+ ===================================================================================
+ Google Analytics code. Remove comments when ready for production
+ ===================================================================================
+ -->
+
+<!--   
+<script>
+var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-XXXXXX']);_gaq.push(['_trackPageview']);_gaq.push(['_trackPageLoadTime']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
+})();
+</script>
+-->
+</body>
+</html>
