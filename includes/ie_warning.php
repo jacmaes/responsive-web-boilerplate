c3e6a83056ad8a9c50c3c058d46611cb146314f5
Jacques <jacmaes@gmail.com>
2013-03-20 20:06:28 +0100

    Removed iewarning.php and renamed it to ie_warning.php

---
+++ includes/ie_warning.php
@@ -0,0 +1,6 @@
+<?php if (($fullbrowser == 'ie6') OR ($fullbrowser == 'ie7')) { ?>
+<div class="iewarning">
+<p><b>Nota Importante :</b> El navegador que usas para consultar esta página Web &mdash;Internet Explorer&mdash; es demasiado antiguo para poder mostrar esta página correctamente. Internet ha cambiado mucho en los últimos años, pero tu navegador no. Te aconsejamos descargar un navegador más moderno y seguro como <a href="http://www.google.es/chrome">Google Chrome</a> o <a href="http://www.firefox.com">Mozilla Firefox</a> para una mejor experiencia en hiedracenters.com, y cualquier otra Web. Es sencillo, rápido, y gratuito. No esperes más, nos lo agradecerás.</p>
+<p>¿No sabes lo que es un <em>navegador</em>? Deja que te lo explique el propio Google <a href="http://www.whatbrowser.org/es/">aquí</a> en un minuto.</p>
+</div>
+<?php }  ?>
